75b6d14616140b971c1e43c5c77378b13167143c
Simon Hirscher <simon.hirsch@libal-tech.de>
2021-01-28 14:10:45 +0100

    clean commit

---
+++ libal-ifc-service-472/Controllers/IfcController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using Xbim.CobieLiteUk;
+using Xbim.Ifc;
+using XbimExchanger.IfcHelpers;
+using XbimExchanger.IfcToCOBieLiteUK;
+
+namespace libal_ifc_service_472
+{
+    public class IfcController : ApiController
+    {
+        // GET api/ifc
+        public IEnumerable<string> Get()
+        {
+
+            var file = @"C:\Users\shirs\OneDrive\Dokumente\libal\cobies\SampleHouse.ifc";
+            using (var stepModel = IfcStore.Open(file))
+            {
+                var facilities = new List<Facility>();
+                var ifcToCoBieLiteUkExchanger = new IfcToCOBieLiteUkExchanger(stepModel, facilities, null, null, null, EntityIdentifierMode.GloballyUniqueIds);
+                facilities = ifcToCoBieLiteUkExchanger.Convert();
+
+                var facility = facilities.ToArray()[0];
+
+                facility.WriteXml(@"C:\Users\shirs\OneDrive\Dokumente\libal\cobies\SampleHouse.xml");
+            }
+
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
---
+++ libal-ifc-service-472/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="libal_ifc_service_472.WebApiApplication" Language="C#" %>
