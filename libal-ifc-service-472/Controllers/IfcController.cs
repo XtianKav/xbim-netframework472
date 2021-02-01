806c2434f0d448be3f7712cb02b0ac92a90cae5a
Simon Hirscher <simon.hirsch@libal-tech.de>
2021-02-01 13:20:01 +0100

    updated to small example

--- libal-ifc-service-472/Controllers/IfcController.cs
+++ libal-ifc-service-472/Controllers/IfcController.cs
@@ -1,45 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using libal.Services;
 using System.IO;
+using System.Threading.Tasks;
 using System.Web.Http;
-using Xbim.CobieLiteUk;
-using Xbim.Ifc;
-using XbimExchanger.IfcHelpers;
-using XbimExchanger.IfcToCOBieLiteUK;
 
 namespace libal_ifc_service_472
 {
     public class IfcController : ApiController
     {
-        // GET api/ifc
-        public IEnumerable<string> Get()
+        // POST api/ifc
+        public async Task<Stream> PostAsync()
         {
+            var ifcStream = await Request.Content.ReadAsStreamAsync();
+            var wexbimStream = await WexbimConverterService.ConvertAsync(ifcStream);
 
-            var file = @"C:\Users\shirs\OneDrive\Dokumente\libal\cobies\ALC_ARC 210127.ifc";
-            using (var stepModel = IfcStore.Open(file))
-            {
-                var facilities = new List<Facility>();
-                var ifcToCoBieLiteUkExchanger = new IfcToCOBieLiteUkExchanger(stepModel, facilities, null, null, null, EntityIdentifierMode.GloballyUniqueIds);
-                facilities = ifcToCoBieLiteUkExchanger.Convert();
-
-                var facility = facilities.ToArray()[0];
-
-                facility.WriteXml(@"C:\Users\shirs\OneDrive\Dokumente\libal\cobies\ALC_ARC 210127.xml");
-
-                var wexBimFilename = Path.ChangeExtension(file, "wexBIM");
-                using (var wexBiMfile = File.Create(wexBimFilename))
-                {
-                    using (var wexBimBinaryWriter = new BinaryWriter(wexBiMfile))
-                    {
-                        stepModel.SaveAsWexBim(wexBimBinaryWriter);
-                        wexBimBinaryWriter.Close();
-                    }
-                    wexBiMfile.Close();
-                }
-            }
-
-            return new string[] { "value1", "value2" };
+            return wexbimStream;
         }
-
     }
+
 }
